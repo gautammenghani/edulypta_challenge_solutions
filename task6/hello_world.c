31d5fa65af9583a4d54d5658afbfe856b3ef5ce6
Gautam Menghani <gum3ng@protonmail.com>
2022-09-11 20:25:29 +0530

    improve the code in task6

--- task6/hello_world.c
+++ task6/hello_world.c
@@ -4,17 +4,7 @@
 
 char *myid = "testId";
 
-MODULE_LICENSE("MIT");
-
-static int device_open(struct inode *inode, struct file *file)
-{
-	return 0;
-}
-
-static int device_release(struct inode *inode, struct file *file)
-{
-	return 0;
-}
+MODULE_LICENSE("GPL v2");
 
 static ssize_t device_read(struct file *filp,  char __user *buffer,
                            size_t length, loff_t *offset)
@@ -30,7 +20,7 @@ static ssize_t device_write(struct file *filp, const char __user *buff,
 	char buffer[10];
 	if(copy_from_user(buffer, buff, len))
 		pr_warn("error in copy_from_user\n");
-	if (!strncmp(buffer, myid, strlen(buffer))) 
+	if (len == strlen(myid) && !strncmp(buffer, myid, strlen(myid))) 
 		return 0;	
 	return -EINVAL;
 }
@@ -38,12 +28,10 @@ static ssize_t device_write(struct file *filp, const char __user *buff,
 struct file_operations fops = {
        .read = device_read,
        .write = device_write,
-       .open = device_open,
-       .release = device_release
 };
 
 struct miscdevice md = {
-	.minor = 0,
+	.minor = MISC_DYNAMIC_MINOR,
 	.name = "edulypta",
 	.fops = &fops,
 };
